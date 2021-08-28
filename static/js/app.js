24ca233ce3795a7f727b2eca4462a6aea5afa558
codestorms37 <46337583+codestorms37@users.noreply.github.com>
2021-08-28 14:56:27 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -4,9 +4,11 @@ const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
+
+
 // 11.5.2 Add forEach to Your Table
 function buildTable(data) {
-    
+
     // First, clear out any existing data
     tbody.html("");
 
@@ -25,3 +27,32 @@ function buildTable(data) {
         );
     });
 }
+
+
+
+// 11.5.3 Add Filters
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // 11.5.4 Use the “If” Statement
+    // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+        // Apply `filter` to the table data to only keep the
+        // rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+};
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
