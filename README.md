2b60f1e175c35657241baf80032577800161c9cc
codestorms37 <46337583+codestorms37@users.noreply.github.com>
2021-08-28 22:29:14 -0500

    Updated functionality and Readme

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # UFOs
+
+## Overview
+
+- The purpose of this project is to deliver a web page to facilitate the presentation and filtering of a database of UFOs sightenings in USA and Canada
+
+## Results
+
+- The original input for was based on text input, but it was replaced with comboboxes.
+- These combo boxes are updated as the page is loaded and the options are automatically adapted to the data in data.js.
+
+![Drop Boxes](./static/images/boxdrop_selection.png)
+
+### Date Example
+
+![Choose Date](./static/images/choose_date.png)
+
+### City Example
+
+![Choose City](./static/images/choose_city.png)
+
+### Filter Example
+
+![Filter Example](./static/images/filters_example.png)
+
+### Data Example
+
+![Data Results](./static/images/data_results.png)
+
+## Summary
+
+### Drawbacks attended
+
+- The original input form was prone to input errors as the user had to type the filters.
+- To avoid the user input error the webpage and app.js was modified to replace the text inputs by dropboxes.
+- The options in the dropboxes are automatically updated using the data in data.js when the page is loaded using:
+  - Arrays
+  - Set constructor
+  - D3
+
+### Further development recomendations
+
+- Date selection is akward as is ordered as text it should be reordered as date.
+- A good graph showing the sightings dates could help to understand the data.
--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,18 @@
-console.log("Inicia")
+console.log("Inicia");
 
 // from data.js
 const tableData = data;
 
 // get table references
-var tbody = d3.select("tbody");
+let tbody = d3.select("tbody");
+
+// get selection references
+let selectDate = d3.select("#datetime");
+let selectCity = d3.select("#city");
+let selectState = d3.select("#state");
+let selectCountry = d3.select("#country");
+let selectShape = d3.select("#shape");
+
 
 function buildTable(data) {
   // First, clear out any existing data
@@ -25,64 +33,120 @@ function buildTable(data) {
   });
 }
 
+function buildOptions(optionsArray,selector) {
+  // First, clear out any existing data
+  selector.html("");
+
+  // Ad Any
+  selector
+      .append("option")
+      .text("any")
+      .property("value","any")
+
+  // Next, loop through each object
+  optionsArray.forEach((myOption) => {
+    // Append an option to select
+    selector
+      .append("option")
+      .text(myOption)
+      .property("value",myOption)
+  });
+}
+
 // 1. Create a variable to keep track of all the filters as an object.
 let filters = {};
 
-
-// 3. Use this function to update the filters. 
+// 3. Use this function to update the filters.
 function updateFilters() {
+  // 4a. Save the element that was changed as a variable.
+  let myElement = d3.select(this);
+  console.log("myElement: ", myElement);
 
-    // 4a. Save the element that was changed as a variable.
-    let myElement = d3.select(this);
-    console.log("myElement: ", myElement)
+  // 4b. Save the value that was changed as a variable.
 
-    // 4b. Save the value that was changed as a variable.
+  let elementValue = myElement.property("value");
+  console.log("elementValue: ", elementValue);
 
-    let elementValue = myElement.property("value");
-    console.log("elementValue: ",elementValue);
+  // 4c. Save the id of the filter that was changed as a variable.
+  let elementId = myElement.attr("id");
+  console.log("elementId: ", elementId);
 
-    // 4c. Save the id of the filter that was changed as a variable.
-    let elementId = myElement.attr("id");
-    console.log("elementId: ",elementId);
-  
-    // 5. If a filter value was entered then add that filterId and value
-    // to the filters list. Otherwise, clear that filter from the filters object.
-    if (elementValue) {
-        filters[elementId] = elementValue;
-    } else {
-        delete filters[elementId];
-    }
-  
-    // 6. Call function to apply all filters and rebuild the table
-    filterTable();
-  
+  // 5. If a filter value was entered then add that filterId and value
+  // to the filters list. Otherwise, clear that filter from the filters object.
+  if (elementValue==="any") {
+    delete filters[elementId];
+  } else {
+    filters[elementId] = elementValue;
   }
-  
-  // 7. Use this function to filter the table when data is entered.
-  function filterTable() {
-  
-    // 8. Set the filtered data to the tableData.
-    let filteredData = tableData;
-  
-    // 9. Loop through all of the filters and keep any data that
-    // matches the filter values
-    for (const key in filters) {
-      if (Object.hasOwnProperty.call(filters, key)) {
-        const value = filters[key];
-        filteredData = filteredData.filter(row => row[key] === value);
-      }
+
+  console.log("filters: ",filters);
+  // 6. Call function to apply all filters and rebuild the table
+  filterTable();
+}
+
+// 7. Use this function to filter the table when data is entered.
+function filterTable() {
+  // 8. Set the filtered data to the tableData.
+  let filteredData = tableData;
+
+  // 9. Loop through all of the filters and keep any data that
+  // matches the filter values
+  for (const key in filters) {
+    if (Object.hasOwnProperty.call(filters, key)) {
+      const value = filters[key];
+      filteredData = filteredData.filter((row) => row[key] === value);
     }
-    
-  
-    // 10. Finally, rebuild the table using the filtered data
-    buildTable(filteredData);
-    console.log("Next Table Build");
-    console.log(filters);
   }
-  
-  // 2. Attach an event to listen for changes to each filter
-  d3.selectAll(".myfilter").on("change", updateFilters);
-  
-  // Build the table when the page loads
-  buildTable(tableData);
-  console.log("First Table Build");
+
+  // 10. Finally, rebuild the table using the filtered data
+  buildTable(filteredData);
+}
+
+
+myKeys = ["datetime","city","state","country","shape"]
+
+let myOptions = {
+  datetime:[],
+  city:[],
+  state:[],
+  country:[],
+  shape:[]
+};
+
+console.log(myOptions);
+
+function createOptions(data) {
+
+  // Loop through each object in the data
+  data.forEach((dataRow) => {
+    // Append each value
+    myKeys.forEach(myKey => {
+      myOptions[myKey].push(dataRow[myKey]);
+    });
+  });
+
+  console.log(myOptions);
+
+  myKeys.forEach(myKey => {
+    // myOptions[myKey] = myOptions[myKey].filter(onlyUnique);
+    myOptions[myKey] = [...new Set(myOptions[myKey])].sort();
+  });
+
+}
+
+function buildAllOptions(){
+  buildOptions(myOptions.datetime,selectDate);
+  buildOptions(myOptions.city,selectCity);
+  buildOptions(myOptions.state,selectState);
+  buildOptions(myOptions.country,selectCountry);
+  buildOptions(myOptions.shape,selectShape);
+};
+
+// 2. Attach an event to listen for changes to each filter
+d3.selectAll(".myfilter").on("change", updateFilters);
+
+// Build the table when the page loads
+buildTable(tableData);
+createOptions(tableData);
+console.log(myOptions);
+buildAllOptions();
